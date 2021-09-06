71050eb6c16b08ca02af8b03640b020a6278ca8a
aaa <aaa@DESKTOP-G20913L>
2021-09-06 18:30:10 +0900

    도전과제

--- chapter9/src/HW/Hw09_06Hard.java
+++ chapter9/src/HW/Hw09_06Hard.java
@@ -7,13 +7,19 @@
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			GridLayout gc = new GridLayout(1,10);
 			Container c = getContentPane();
-			
+			Color[] col= {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, 
+					Color.CYAN, Color.BLUE, Color.MAGENTA, Color.GRAY, Color.PINK, Color.LIGHT_GRAY};
+
+	
+
 			c.setLayout(gc);
 			for(int i =1;i<10;i++) {
 				String txt = Integer.toString(i);
 					JButton bt = new JButton(txt);
 					c.add(bt);
-				} 
+					bt.setBackground(col[i]);	
+			} 
+			
 
 			setSize(800,350);
 			setVisible(true);
